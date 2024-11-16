5e9c0d150e87c988d7c433d3146cfc7545addaa7
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-11-16 14:45:54 +0530

    array  Methods

---
+++ ArrayMethod.js
@@ -0,0 +1,47 @@
+
+// let a = Number("123")
+// // let c = parseInt("100.01")
+// let c =parseFloat("100.20")
+// console.log(c)
+// let b = String(123)
+// console.log(typeof(b));
+
+// var fe = arr.forEach(function(element,index)
+
+// {
+//     console.log(element*2)
+// })
+
+
+
+
+// var arr =[1,2,3,4,5,5];
+// var fe = arr.forEach(function(element,index)
+// {
+//     console.log(element*2)
+// })
+
+const data =[
+    {
+        name:"Maggi",
+        id:142
+    },
+    {
+        name:"yipee",
+        id:14
+    },
+
+    {
+        name:"soup",
+        id:12
+    },
+    {
+        name:"Maggi",
+        id:10
+    },
+]
+
+const be = data.forEach(function(id)
+{
+    console.log(id)
+})
