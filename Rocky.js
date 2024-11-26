cc2f6022efc97d6a983c68c7d7b9dee4d7afcb17
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-11-26 09:24:49 +0530

    essagejavaScript File

---
+++ Rocky.js
@@ -0,0 +1,47 @@
+const data =[
+    {
+        name:"Maggi",
+        id:142
+    },
+    {
+        name:"yipee",
+        id:14
+    },
+
+    {
+        name:"soup",
+        id:12
+    },
+    {
+        name:"Maggi",
+        id:10
+    },
+]
+
+
+// if you want print the  only items you havw ti change the object array
+// And the object array .properties
+const be = data.forEach(function(item)
+{
+    console.log(item.name)
+})
+
+
+// var arr =[1,2,3,4,5,5];
+
+// var arr2 = arr.map(function(element,index)
+// {
+//     return element*2
+// })
+
+// console.log(arr);
+// console.log(arr2);
+
+
+// Increatements
+
+// var x =10;
+// var y =x++ + ++x - --x+x-- ;
+
+// console.log(x);
+// console.log(y);
