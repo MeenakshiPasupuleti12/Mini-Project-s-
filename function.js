ce549eac1528bed68967d55dbc13afca37eaf6bf
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-12-12 12:15:50 +0530

    essagefunctions

---
+++ function.js
@@ -0,0 +1,14 @@
+// named Function
+function f1(x,y)//parameters
+{
+    console.log("Hello world")
+    console.log(x+y)
+}
+f1(2,3)//Arguments
+
+
+//Async function
+async function(x)
+{
+   console.log(x)
+}
