02962c6a8e9dc3f01aa3e6298c0534b47cdf3e52
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2025-01-07 09:16:32 +0530

    essagevaribles and objects

---
+++ var.js
@@ -0,0 +1,17 @@
+// var a =10;
+// var b=20;
+// console.log(a+b);
+
+
+//Objects
+
+let bike={
+    name:"Royal Enfield",
+    topSpeed:120,
+}
+console.log(bike.name);
+bike.name="KTM";
+console.log(bike.name);
+console.log(bike);
+bike.topSpeed=150;
+console.log(bike);
