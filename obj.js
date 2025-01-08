458a55618937fdfe37b1892883108660977052c6
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2025-01-08 13:25:38 +0530

    essage  Objects of the students data

---
+++ obj.js
@@ -0,0 +1,49 @@
+function fun()
+{
+let Student=[
+    {
+        name:"ANR",
+        Id:501,
+        PhNo:9494949494,
+        Sec:"Cse"
+    },
+    {
+        name:"ANR",
+        Id:501,
+        PhNo:9494949494,
+        Sec:"Cse"
+    },
+    {
+        name:"Mounika",
+        Id:502,
+        PhNo:9494949494,
+        Sec:"Cse"
+    },
+    {
+        name:"Basava",
+        Id:503,
+        PhNo:9494949494,
+        Sec:"Cse"
+    },
+    {
+        name:"Ambica",
+        Id:504,
+        PhNo:9494949494,
+        Sec:"Cse"
+    },
+    {
+        name:"Karthik",
+        Id:505,
+        PhNo:9494949494,
+        Sec:"Cse"
+    },
+    {
+        name:"Amurutha",
+        Id:506,
+        PhNo:9494949494,
+        Sec:"Cse"
+    }
+]
+console.log(Student[0]);
+}
+fun();
