fef6235e6a2169f241d4748cfed9c5d405dbe633
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2025-10-25 09:01:57 +0530

    Update ArrayMethod.js

--- ArrayMethod.js
+++ ArrayMethod.js
@@ -2,7 +2,7 @@ let a = Number("123")
  let c = parseInt("100.01")
  let c =parseFloat("100.20")
 console.log(c)
-// let b = String(123)
+/ let b = String(123)
 // console.log(typeof(b));
 
 // var fe = arr.forEach(function(element,index)
